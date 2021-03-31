1abdceeb500bf23d86171a2f0e171b5337ef254f
danorel <mail.ordan@gmail.com>
2021-03-31 18:01:34 +0300

    [Gitignore + Setup]: Created basic setup with Objective-C lang.

---
+++ Window-Management/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Window-Management
+//
+//  Created by Dan Orel on 31.03.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
